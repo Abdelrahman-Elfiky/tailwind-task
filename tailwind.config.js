4544d5b850b9e6bc73f49a70e65777ea1c23e14f
Abdelrahman-Elfiky <abdelrahman.elfiky152@gmail.com>
2025-08-02 23:03:53 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./public/*.html"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'text-color': '#272D37',
+        'secondry-color': '#A5ACBA'
+      },
+    },
+  },
+  plugins: [],
+}
+
